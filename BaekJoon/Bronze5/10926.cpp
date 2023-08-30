ebb4e0c1a2fd4cecb4fd5d8cd0c872d9155b8c10
고은서 <97677518+godori1012@users.noreply.github.com>
2023-08-30 20:42:21 +0900

    Solve 10926.cpp

---
+++ BaekJoon/Bronze5/10926.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string nickname;
+    cin >> nickname;
+    
+    string surp = nickname + "??!";
+    
+    cout << surp;
+
+
+    return 0;
+}
