77cc44118018cba588260927c4d197f8154e9388
고은서 <97677518+godori1012@users.noreply.github.com>
2023-08-30 20:48:49 +0900

    Solve 18108.cpp

---
+++ BaekJoon/Bronze5/18108.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int seoYear, bulYear;
+    cin >> bulYear;
+    
+    seoYear = bulYear - 543;
+    
+    cout << seoYear;
+    
+
+    return 0;
+}
