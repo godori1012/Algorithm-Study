f2da109f8fafe367f7ed55b0cb2d4f0a70a4c6be
Eunseo <97677518+godori1012@users.noreply.github.com>
2024-03-23 22:15:26 +0900

    [level 2] Title: 3 x n 타일링, Time: 0.92 ms, Memory: 3.72 MB -BaekjoonHub

---
+++ 프로그래머스/2/12902. 3 x n 타일링/3 x n 타일링.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int n) {
+    if(n%2!=0) return 0;
+    
+    long long dp[5001]={0,};
+    dp[0]=1;
+    dp[2]=3;
+    
+    for(int i=4; i<=n; i+=2){
+        dp[i]=dp[i-2]*3;
+        
+        for(int j=i-4; j>=0; j-=2){
+            dp[i]+=dp[j]*2;
+        }
+        dp[i]%=1000000007;
+    }
+    return dp[n];
+}
