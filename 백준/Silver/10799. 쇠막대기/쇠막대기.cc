cb8ee20866ad7b0978cd2762e74d244a2f47e9d6
Eunseo <97677518+godori1012@users.noreply.github.com>
2024-03-24 12:21:19 +0900

    [Silver II] Title: 쇠막대기, Time: 4 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.cc
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.cc
@@ -3,33 +3,30 @@
 using namespace std;
 
 int solution(string input){
-    
     stack<char> st;
     int sum=0;
-    
     for(int i=0; i<input.size(); i++){
-        if(input[i]=='('){   // 열린 괄호
+        if(input[i]=='('){
             st.push('(');
-        } else{             // 닫힌 괄호
+        } else{
             st.pop();
-            if(input[i-1]=='('){ // 이전괄호=> 열린 괄호 (레이저)
+            if(input[i-1]=='('){
                 sum+=st.size();
-            } else{             // 이전괄호 => 닫힌 괄호
+            } else{
                 sum++;
             }
-            
         }
     }
+    
     return sum;
 }
 
 int main()
 {
     string input;
-    cin >> input;
+    cin>>input;
     
     cout<<solution(input);
 
     return 0;
 }
-
